215781a0bec7d3e2667f9653ed3db8a3b1b3b318
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-05-12 04:49:10 +0300

    Fix: handle returned response from FollowUnfolow api

--- src/app/(components)/profile/FollowUnFollowBtn.tsx
+++ src/app/(components)/profile/FollowUnFollowBtn.tsx
@@ -1,23 +1,25 @@
 "use client"
 
-import { LoggedUserProfile, UserProfile } from "@/app/types/user.types";
+import { UserProfile } from "@/app/types/user.types";
 import { followAndUnfollowUser } from "../SuggestedUsers/SuggestedUsers.actions";
 import { useState } from "react";
+import { toast } from "@heroui/react";
 
-export default function FollowUnFollowBtn({userProfile} : {userProfile : UserProfile | LoggedUserProfile}) {
+export default function FollowUnFollowBtn({ userProfile }: { userProfile: UserProfile}) {
 
-    const [following, setFollowing] = useState<boolean>('isFollowing' in userProfile ? userProfile.isFollowing : false);
-    
-   async function handleFollowUnfollowUser(){
-    const isfollow = await followAndUnfollowUser(userProfile.user._id);
-    if(isfollow){
-        setFollowing(isfollow);
+    const [following, setFollowing] = useState<boolean>(userProfile.isFollowing);
+    async function handleFollowUnfollowUser() {
+        const isfollow = await followAndUnfollowUser(userProfile.user._id);
+        try {
+            setFollowing(isfollow);
+        }
+        catch (_) {
+            toast.danger("Network failed!");
+        }
     }
-   }
-
     return (
         <button
-        onClick={handleFollowUnfollowUser}
+            onClick={handleFollowUnfollowUser}
             className={`flex items-center cursor-pointer gap-2 rounded-2xl px-5 py-2.5 text-sm font-medium transition ${following
                 ? "border border-white/10 bg-white/5 text-white hover:bg-white/10"
                 : "bg-indigo-500 text-white hover:bg-indigo-400"}`}>
@@ -26,3 +28,4 @@ export default function FollowUnFollowBtn({userProfile} : {userProfile : UserPro
         </button>
     )
 }
+
